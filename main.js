1b946b5ffb67eedde9d293b4494f63815e0332a6
uzbcrimson <joraqulovcrimson@gmail.com>
2022-04-19 08:04:06 +0300

    js_oop

---
+++ main.js
@@ -0,0 +1,61 @@
+// function Odam(ismi,familya,yoshi){
+//     this.ismi=ismi;
+//     this.familya=familya;
+//     this.yoshi=yoshi;
+// }
+
+// var odam1 = new Odam("Jasur","Jo'raqulov",12);
+
+// console.log(odam1)
+
+
+
+function Car(model,rangi,tezligi){
+    this.model=model;
+    this.rangi=rangi;
+    this.tezligi=tezligi;
+}
+
+var car1 = new Car("mercedes E230","qora",260)
+
+console.log(car1)
+
+function Car(model,rangi,tezligi){
+    this.model=model;
+    this.rangi=rangi;
+    this.tezligi=tezligi;
+}
+
+var car2 = new Car("bugatti","oq",400)
+
+console.log(car2)
+
+function Car(model,rangi,tezligi){
+    this.model=model;
+    this.rangi=rangi;
+    this.tezligi=tezligi;
+}
+
+var car3 = new Car("ferrari","qizil",300)
+
+console.log(car3)
+
+function Car(model,rangi,tezligi){
+    this.model=model;
+    this.rangi=rangi;
+    this.tezligi=tezligi;
+}
+
+var car4 = new Car("wolsvagen","sariq",230)
+
+console.log(car4)
+
+function Car(model,rangi,tezligi){
+    this.model=model;
+    this.rangi=rangi;
+    this.tezligi=tezligi;
+}
+
+var car5 = new Car("matiz","oq",600 )
+
+console.log(car5)
